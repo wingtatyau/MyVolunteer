15b84a26216beb81b064f129e25d4d74c9f79ec3
AltmanWong <mr.altmanwong@gmail.com>
2016-03-23 13:37:48 +0800

    QuestList RecyclerView Implementation

--- app/src/main/java/com/eeproject/myvolunteer/myvolunteer/adapter_QuestListRecyclerView.java
+++ app/src/main/java/com/eeproject/myvolunteer/myvolunteer/adapter_QuestListRecyclerView.java
@@ -1,5 +1,6 @@
 package com.eeproject.myvolunteer.myvolunteer;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,17 +15,17 @@ import java.util.List;
  */
 public class adapter_QuestListRecyclerView extends RecyclerView.Adapter<adapter_QuestListRecyclerView.ViewHolder>{
     private List<quest> mQuest;
-
-
+    private Context context;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView title, expirydate;
+        TextView title, expirydate, info;
         ImageView questicon;
         public ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.textView_title);
             expirydate = (TextView) itemView.findViewById(R.id.textView_expirydate);
             questicon = (ImageView) itemView.findViewById(R.id.imageView_questicon);
+            info = (TextView) itemView.findViewById(R.id.textView_info);
         }
     }
 
@@ -36,6 +37,7 @@ public class adapter_QuestListRecyclerView extends RecyclerView.Adapter<adapter_
     public adapter_QuestListRecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cardview, parent, false);
+        context = parent.getContext();
         // set the view's size, margins, paddings and layout parameters
         ViewHolder vh = new ViewHolder(v);
         return vh;
@@ -46,7 +48,9 @@ public class adapter_QuestListRecyclerView extends RecyclerView.Adapter<adapter_
         quest temp = mQuest.get(position);
         holder.title.setText(temp.getTitle());
         holder.expirydate.setText(temp.getExpirydate());
-        int id = R.drawable.nomorewtf;
+        holder.info.setText(temp.getInfo());
+
+        int id = context.getResources().getIdentifier(temp.getIcon(), "drawable", "com.eeproject.myvolunteer.myvolunteer");
         holder.questicon.setImageResource(id);
     }
 
